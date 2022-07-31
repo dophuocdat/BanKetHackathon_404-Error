866e152af27591591548fab2454d7cc6b5d0fe50
dophuocdat <Datdo775@gmail.com>
2022-07-31 08:06:48 +0700

    Initial commit.

---
+++ src/Coursework_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { Coursework_backend } from "../../declarations/Coursework_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Coursework_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
